0b57dfa7e838547024e827c22d8cb04ab984c27b
Kamil Żmuda <kamilos1946@gmail.com>
2019-03-08 12:47:51 +0100

    Update and Delete work without refresh for AnimeList. TextBoxes for Add New Anime.

--- MAL-Excel App/ViewModels/AnimeListViewModel.cs
+++ MAL-Excel App/ViewModels/AnimeListViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using GalaSoft.MvvmLight.CommandWpf;
 using MAL_Excel_App.Models;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows.Input;
 
@@ -8,6 +9,7 @@ namespace MAL_Excel_App.ViewModels
 {
     class AnimeListViewModel : BaseViewModel
     {
+        public AnimeListFromExcel AnimeInfo { get; set; } = new AnimeListFromExcel();
         public AnimeListViewModel(MalAppDBEntities context) : base(context)
         {
             FillAnimeList();
@@ -37,21 +39,64 @@ namespace MAL_Excel_App.ViewModels
                 OnPropertyChanged();
             }
         }
+        private AnimeListFromExcel _selectedAnime;
+        public AnimeListFromExcel SelectedAnime
+        {
+            get { return _selectedAnime; }
+            set
+            {
+                _selectedAnime = value;
+                OnPropertyChanged();
+            }
+        }
         #region Insert, Update, Delete
-        //private RelayCommand _addAnimeCommand;
-        //public ICommand AddAnimeCommand => _addAnimeCommand ?? (_addAnimeCommand = new RelayCommand(() =>
-        //{
-        //    Context.AnimeListFromExcels.Add(new AnimeListFromExcel {
-        //        Title = titleTextBoxNew.Text,
-        //        Score = int.Parse(scoreTextBoxNew.Text),
-        //        Watched_episodes = int.Parse(watched_episodes_or_read_chaptersTextBoxNew.Text),
-        //        Episodes = int.Parse(episodes_or_chaptersTextBoxNew.Text),
-        //        Type = typeTextBoxNew.Text,
-        //        My_status = my_statusTextBoxNew.Text,
-        //        Genres = genresTextBoxNew.Text,
-        //        Additional_information = additional_informationTextBoxNew.Text
-        //    });
-        //}, () => { }));
+        private RelayCommand _addAnimeCommand;
+        public ICommand AddAnimeCommand => _addAnimeCommand ?? (_addAnimeCommand = new RelayCommand(() =>
+        {
+            Context.AnimeListFromExcels.Add(new AnimeListFromExcel
+            {
+                Title = AnimeInfo.Title,
+                Score = AnimeInfo.Score,
+                Watched_episodes = AnimeInfo.Watched_episodes,
+                Episodes = AnimeInfo.Episodes,
+                Type = AnimeInfo.Type,
+                My_status = AnimeInfo.My_status,
+                Genres = AnimeInfo.Genres,
+                Additional_information = AnimeInfo.Additional_information
+            });
+            Context.SaveChanges();
+        },
+        () => { return true; }));
+        private RelayCommand _updateAnimeCommand;
+        public ICommand UpdateAnimeCommand => _updateAnimeCommand ?? (_updateAnimeCommand = new RelayCommand(() =>
+        {
+            Context.SaveChanges();
+        },
+        () =>
+        {
+            if (SelectedAnime == null)
+                return false;
+            return true;
+        }));
+        private RelayCommand _deleteAnimeCommand;
+        public ICommand DeleteAnimeCommand => _deleteAnimeCommand ?? (_deleteAnimeCommand = new RelayCommand(() =>
+        {
+            Context.AnimeListFromExcels.Remove(SelectedAnime);
+            Context.SaveChanges();
+        },
+        () => SelectedAnime != null));
         #endregion
     }
+    //internal class AnimeInfo
+    //{
+    //    public int ID { get; set; }
+    //    public string Title { get; set; }
+    //    public int Score { get; set; }
+    //    public int Watched_episodes { get; set; }
+    //    public int Episodes { get; set; }
+    //    public string Type { get; set; }
+    //    public string My_status { get; set; }
+    //    public string Genres { get; set; }
+    //    public string Additional_information { get; set; }
+    //}
 }
